c205455faa9da59e5e4731b26145c3f01372a08b
yyj <yiyajing0921@yeah.net>
2020-11-09 13:57:39 +0800

    第一次创建TAG

--- app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
+++ app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //这是第一版本内容，将要提交TAG，以后就是TAG-1版本。第一版常驻内容
     }
 }
