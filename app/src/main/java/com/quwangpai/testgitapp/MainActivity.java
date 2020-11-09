b30bb9a54b832d61f37cab360f66f881c0e0e09a
yyj <yiyajing0921@yeah.net>
2020-11-09 14:36:04 +0800

    tag分支

--- app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
+++ app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //这是第一版本内容，将要提交TAG，以后就是TAG-1版本。第一版常驻内容
         //修改TAG-1内容，并提交到分支
+        //添加一行分支--15行
     }
 }
