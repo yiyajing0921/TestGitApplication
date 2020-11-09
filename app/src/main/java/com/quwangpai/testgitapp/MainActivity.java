cb2259b061dd06dbab75cdbc8c825f3fa773e7a8
yyj <yiyajing0921@yeah.net>
2020-11-09 14:18:39 +0800

    提交TAG-1第一次修改

--- app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
+++ app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //这是第一版本内容，将要提交TAG，以后就是TAG-1版本。第一版常驻内容
+        //修改TAG-1内容，并提交到分支
     }
 }
