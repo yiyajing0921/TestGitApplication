00de53dabb68dba863e2be6158c7fc0535ff835a
yyj <yiyajing0921@yeah.net>
2020-11-09 14:32:11 +0800

    上传主分支内容-未合并

--- app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
+++ app/src/main/java/com/quwangpai/testgitapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //这是第一版本内容，将要提交TAG，以后就是TAG-1版本。第一版常驻内容
+        //修改主分支内容，合并tag分支内容
     }
 }
